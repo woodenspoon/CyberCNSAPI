8c293bb85f6572cf676156d7238b28f52850ad34
Christian Nancy <christian@wooden-spoon.com>
2023-07-25 12:48:51 -0700

    Added code to handle manual settings

--- Public/Start-CCNSConnection.ps1
+++ Public/Start-CCNSConnection.ps1
@@ -7,9 +7,9 @@ Initiates a connection to the CyberCNS API
 
 This function will connect to the CyberCNS API on your dashboard.
 
-The function will read the parameters from the WSTBlobData 'CyberCNSAPI/Settings'.
+The function will read the parameters from the WSTBlobData 'CyberCNSAPI/Settings', unless manual parameters are passed.
 
-Returns the headers to use in subsequent calls if successful, $null if the connection was not successful.
+Returns $true if successful, $false if the connection was not successful.
 
 .EXAMPLE
 
@@ -17,39 +17,68 @@ Start-CCNSConnection
 
 #>
 Function Start-CCNSConnection {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'Default')]
     param(
-        [switch]$VerifyLogin
+        [Parameter(ParameterSetName = 'Default')]
+        [Parameter(ParameterSetName = 'Manual')]
+        [switch]$VerifyLogin,
+        [Parameter(Mandatory = $true, ParameterSetName = 'Manual')]
+        [string]$ManualClientID,
+        [Parameter(Mandatory = $true, ParameterSetName = 'Manual')]
+        [string]$ManualClientSecret,
+        [Parameter(Mandatory = $true, ParameterSetName = 'Manual')]
+        [string]$ManualBaseURI,
+        [Parameter(Mandatory = $true, ParameterSetName = 'Manual')]
+        [string]$ManualTenantName
     )
 
-    if ((-not $VerifyLogin) -and ($script:ccnsBaseURI) -and ($script:ccnsTenantName) -and ($script:ccnsBasicAuth)) {
+    switch ($PsCmdlet.ParameterSetName) {
+        "Manual" {
+            Write-Verbose "Manual parameters passed, using those to make a connection."
 
-        Write-Verbose "Module variables already set, skipping."
-        return $true
+            $ClientID = $ManualClientID
+            $ClientSecret = $ManualClientSecret
+            $BaseURI = $ManualBaseURI
+            $TenantName = $ManualTenantName
 
-    }
-    
-    # Clear the script variables so that no other cmdLet can use them until we have confirmed we have good values
-    $script:ccnsBaseURI = $null
-    $script:ccnsTenantName = $null
-    $script:ccnsBasicAuth = $null
+            # Force a new connection with the manual parameters
+            $VerifyLogin = $true
+            break
+        }
 
-    # Read the settings from the blob
-    $Settings = Get-CCNSSettings
-    if (-Not ($Settings)) {
-        Write-Error "Unable to read settings from WSTBlobData 'CyberCNSAPI/Settings'"
-        return $false
-    }
+        default { 
 
-    $Credentials = $Settings.Credentials
-    $BaseURI = $Settings.BaseURI
-    $TenantName = $Settings.TenantName
+            if ((-not $VerifyLogin) -and ($script:ccnsBaseURI) -and ($script:ccnsTenantName) -and ($script:ccnsBasicAuth)) {
 
-    if ($Credentials) {
-
-        $ClientID = $Credentials.UserName
-        $ClientSecret = $Credentials.GetNetworkCredential().Password
-
+                Write-Verbose "Module variables already set, skipping."
+                return $true
+        
+            }
+            
+            # Clear the script variables so that no other cmdLet can use them until we have confirmed we have good values
+            $script:ccnsBaseURI = $null
+            $script:ccnsTenantName = $null
+            $script:ccnsBasicAuth = $null
+        
+            # Read the settings from the blob
+            $Settings = Get-CCNSSettings
+            if (-Not ($Settings)) {
+                Write-Error "Unable to read settings from WSTBlobData 'CyberCNSAPI/Settings', please use Set-CCNSConnection"
+                return $false
+            }
+        
+            $Credentials = $Settings.Credentials
+            $BaseURI = $Settings.BaseURI
+            $TenantName = $Settings.TenantName
+        
+            if ($Credentials) {
+        
+                $ClientID = $Credentials.UserName
+                $ClientSecret = $Credentials.GetNetworkCredential().Password
+        
+            }
+        
+        }
     }
 
     if ((-Not $ClientID) -Or (-Not $ClientSecret) -Or (-Not $BaseURI) -Or (-Not $TenantName)) {
